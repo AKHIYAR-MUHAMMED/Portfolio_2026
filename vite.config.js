99eea3b855bb5b7b7e79bd37e1b45f8893d6ac3a
Akhiyar Muhammed <akhiyaarmuhammed123@gmail.com>
2026-07-01 00:28:24 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: "/Portfolio_2026/",
   plugins: [react()],
   define: {
     'process.env': {}
